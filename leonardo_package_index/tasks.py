ebc72044e4d1041c82b8a0055adde991b2cfd4ab
Michael Kutý <6du1ro.n@gmail.com>
2015-10-09 15:16:36 +0200

    catch result from celery task

--- leonardo_package_index/tasks.py
+++ leonardo_package_index/tasks.py
@@ -2,9 +2,11 @@ from __future__ import absolute_import
 
 from celery import shared_task
 from django.core import management
+from leonardo.decorators import catch_result
 
 
 @shared_task
+@catch_result
 def update_all_packages():
-    management.call_command('update_all_packages', interactive=False)
+    management.call_command('update_all_packages')
     return {'result': 'Update packages OK'}
